f574e1c303de3152af1c61e5a353d4b2ee52fc20
ghdpreto <ghdpreto@gmail.com>
2020-06-03 00:38:08 -0300

    ajuste para envio de informaçoẽs do estado

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -6,7 +6,6 @@ const ufSelect = document.querySelector("select[name=uf]")
 // document
 //      .querySelector("select[name=uf]")
 //      .addEventListener("change", getCities)
-
 ufSelect.addEventListener("change", getCities)
 
 
@@ -27,10 +26,18 @@ function populateUFs() {
 
 function getCities(event) {
     const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
 
     //pega o value do select estado
     const ufValue = event.target.value
 
+    //verificar o valor selecionado (0 a 26)
+    const indexOfSelectedState = event.target.selectedIndex
+
+    //armazena o valor em texto no input escondido
+    stateInput.value = event.target.options[index].text
+
+    //url para acesso a api
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
     fetch(url)
@@ -41,11 +48,11 @@ function getCities(event) {
                 citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
             }
 
+            //habilita a caixa de seleçao
             citySelect.disabled = false
 
         } )
 }
-
 
 
 //carrega as ufs
