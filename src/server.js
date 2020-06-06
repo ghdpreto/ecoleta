10326eb87d5f0badd6ca692b62b9af5bba65ae16
ghdpreto <ghdpreto@gmail.com>
2020-06-05 22:45:59 -0300

    configurado as paginas com o nunjucks

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const express = require("express"); //usando o express
 const server = express() //executando a express
 
+const nunjucks = require("nunjucks") //usando nunjucks
+
+//configurando o nunjucks
+nunjucks.configure("src/views", {
+    express: server,
+    nocache: true
+})
+
 // configurar pasta public (css, js etc..)
 server.use(express.static("public"))
 
@@ -9,15 +17,35 @@ server.listen(3000)
 
 console.log("Server on")
 
-// configurando rotas (caminhos)
-// pagina incial
+// configurando rotas
+// paginal inicial
 server.get("/", function (req, res) {
-    //enviando a pagina inicial
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.njk", {
+        title: "teste"
+    })
 })
 
-//pagina create-point
+// pagina create-point
 server.get("/create-point", function (req, res) {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.njk")
 })
+
+// pagina search-result
+server.get("/search", function (req, res) {
+    return res.render("search-results.njk")
+})
+
+
+
+// configurando rotas (caminhos) sem nunjucks
+// pagina incial
+// server.get("/", function (req, res) {
+//     //enviando a pagina inicial
+//     return res.sendFile(__dirname + "/views/index.html")
+// })
+
+// //pagina create-point
+// server.get("/create-point", function (req, res) {
+//     return res.sendFile(__dirname + "/views/create-point.html")
+// })
 
